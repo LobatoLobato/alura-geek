5f6b281a8dabdf85b699e094f0127e627463b7e1
LobatoLobato <felipelobato.rj@gmail.com>
2022-10-11 15:34:34 -0300

    Added form validation to Login Page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,50 +2,50 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: () =>
-      import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
-  },
-  {
-    path: "/todosprodutos",
-    name: "produtos",
-    component: () =>
-      import(
-        /* webpackChunkName: "produtos" */ "../views/TodosProdutosView.vue"
-      ),
-  },
-  {
-    path: "/adicionarProduto",
-    name: "adicionarProduto",
-    component: () =>
-      import(
-        /* webpackChunkName: "adicionar-produto" */ "../views/AdicionaProdutoView.vue"
-      ),
-  },
-  {
-    path: "/produto",
-    name: "produto",
-    component: () =>
-      import(/* webpackChunkName: "produto" */ "../views/ProdutoView.vue"),
-  },
+	{
+		path: "/",
+		name: "home",
+		component: HomeView,
+	},
+	{
+		path: "/about",
+		name: "about",
+		component: () =>
+			import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: () =>
+			import(/* webpackChunkName: "login" */ "../views/LoginView.vue"),
+	},
+	{
+		path: "/todosprodutos",
+		name: "produtos",
+		component: () =>
+			import(
+				/* webpackChunkName: "produtos" */ "../views/TodosProdutosView.vue"
+			),
+	},
+	{
+		path: "/adicionarProduto",
+		name: "adicionarProduto",
+		component: () =>
+			import(
+				/* webpackChunkName: "adicionar-produto" */ "../views/AdicionaProdutoView.vue"
+			),
+	},
+	{
+		path: "/produto",
+		name: "produto",
+		component: () =>
+			import(/* webpackChunkName: "produto" */ "../views/ProdutoView.vue"),
+	},
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+	history: createWebHistory(process.env.BASE_URL),
+	routes,
 });
-
+router.push;
 export default router;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,48 +1,102 @@
 <template>
-  <TheHeader></TheHeader>
-  <div class="login">
-    <div class="loginFormContainer">
-      <form>
-        <h2>Iniciar Sessão</h2>
-        <input type="email" placeholder="Escreva seu email" required />
-        <input type="password" placeholder="Escreva sua senha" required />
-        <router-link to="/todosprodutos"
-          ><button class="botaoPrimario w-[109px] desktop:w-full">
-            Entrar
-          </button></router-link
-        >
-      </form>
-    </div>
-  </div>
+	<TheHeader></TheHeader>
+	<div class="login">
+		<div class="flex h-11 w-full items-center justify-center desktop:h-24">
+			<VErrorBox v-show="showError" :error-msg="errorMsg"></VErrorBox>
+		</div>
+
+		<div class="loginFormContainer">
+			<form>
+				<h2>Iniciar Sessão</h2>
+				<input
+					type="text"
+					placeholder="Escreva seu email"
+					id="email"
+					required
+				/>
+				<input
+					type="password"
+					placeholder="Escreva sua senha"
+					id="password"
+					required
+				/>
+				<button
+					class="botaoPrimario w-[109px] desktop:w-full"
+					@click="validateInput"
+					type="button"
+				>
+					Entrar
+				</button>
+			</form>
+		</div>
+	</div>
 </template>
+
 <script lang="ts">
 import TheHeader from "@/components/TheHeader.vue";
-export default {
-  components: {
-    TheHeader,
-  },
-};
+import VErrorBox from "@/components/VErrorBox.vue";
+import router from "@/router";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	data() {
+		return {
+			showError: false,
+			errorMsg: [""],
+		};
+	},
+	methods: {
+		validateInput() {
+			const emailRegex = /.+@[a-z]+\.[a-z]+/gim;
+			const email = document.getElementById("email") as HTMLInputElement;
+			const password = document.getElementById("password") as HTMLInputElement;
+
+			const emailIsValid = emailRegex.test(email.value);
+			const passwordIsValid = password.value.length >= 8;
+
+			if (emailIsValid && passwordIsValid) {
+				this.showError = false;
+				router.push("/todosprodutos");
+				return;
+			}
+
+			this.showError = true;
+
+			this.errorMsg = [];
+			if (!emailIsValid) {
+				this.errorMsg.push("Insira um endereço de email valido");
+			}
+			if (!passwordIsValid) {
+				this.errorMsg.push("Insira uma senha valida");
+			}
+		},
+	},
+	components: {
+		TheHeader,
+		VErrorBox,
+	},
+});
 </script>
 
 <style lang="scss">
 .login {
-  @apply relative items-center bg-[#f5f5f5] py-11 desktop:py-24;
+	@apply relative flex flex-col items-center bg-[#f5f5f5] pb-11 desktop:pb-24;
 }
 .loginFormContainer {
-  @apply relative mx-auto h-[189px] w-[275px] 
+	@apply relative mx-auto h-[189px] w-[275px] 
   desktop:h-[291px] desktop:w-[423px];
-  form {
-    @apply relative mx-auto grid h-full w-full 
+	form {
+		@apply relative mx-auto grid h-full w-full 
     grid-rows-3 gap-4  text-center  desktop:gap-6;
-    grid-template-rows: min-content auto auto min-content;
-    h2 {
-      @apply font-raleway text-xl font-bold text-[#464646];
-    }
-    input {
-      @apply relative mb-2 block h-full w-full border-none 
+		grid-template-rows: min-content auto auto min-content;
+		h2 {
+			@apply font-raleway text-xl font-bold text-[#464646];
+		}
+		input {
+			@apply relative mb-2 block h-full w-full border-none 
       bg-white px-3 font-raleway text-base font-normal
       text-[#A2A2A2] shadow-sm shadow-[#0000005e] outline-none;
-    }
-  }
+		}
+	}
 }
 </style>
